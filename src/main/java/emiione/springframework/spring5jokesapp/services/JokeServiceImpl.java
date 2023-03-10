95e82e78c366119e6692d591c61f043c1d8e152f
rmionescu <rmionescu@yahoo.com>
2023-03-10 13:06:07 +0200

    Spring Service Layer

---
+++ src/main/java/emiione/springframework/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,23 @@
+package emiione.springframework.spring5jokesapp.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+	// Instance variables (Properties)
+	private final ChuckNorrisQuotes chuckNorrisQuotes;
+	
+	// Constructor
+	public JokeServiceImpl() {
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	}
+
+	@Override
+	public String getJoke() {
+		return chuckNorrisQuotes.getRandomQuote();
+	}
+
+}
